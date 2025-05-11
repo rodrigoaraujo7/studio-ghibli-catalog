0ffdc8987298112391cd278dd097bc59614b4433
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-11 13:36:37 -0300

    feat: filtering films and handle clear

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps = CustomProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button = ({ children, variant, disabled, className, color, ...props }: ButtonProps) => {
   return (
     <button
-      className={`font-medium text-base h-9 w-full flex justify-center items-center gap-2 px-3 py-1.5 rounded-md self-end cursor-pointer transition-all ${variant === "contained" && `${color ? color : `bg-black`} text-white`} ${variant === "outlined" && `border-1 border-gray-200 text-black`} ${disabled && 'grayscale-100'} ${className}`}
+      className={`font-medium text-base h-9 w-full flex justify-center items-center gap-2 px-3 py-1.5 rounded-md cursor-pointer transition-all ${variant === "contained" && `${color ? color : `bg-black`} text-white`} ${variant === "outlined" && `border-1 border-gray-200 bg-white text-black`} ${disabled && 'grayscale-100'} ${className}`}
       {...props}
     >
       {children}
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,24 +1,54 @@
+import { useMemo, useState } from "react"
+
 import { SearchInput } from "./SearchInput"
 import { FilmCard } from "./FilmCard"
 
 import { useFilmContext } from "../context/FilmContext"
+import { Button } from "./Button"
 
 export const Catalog = () => {
+  const [searchInputValue, setSearchInputValue] = useState<string>("")
+
   const { films } = useFilmContext()
+
+  const filteredFilms = useMemo(() => {
+    return films.filter((film) =>
+      film.title.toLowerCase().includes(searchInputValue.toLowerCase())
+    )
+  }, [films, searchInputValue])
+
+  const clearFilter = () => {
+    setSearchInputValue("")
+  }
 
   return (
     <div className="space-y-6">
       <SearchInput
         placeholder="Search movies..."
+        value={searchInputValue}
+        onChange={(e) => setSearchInputValue(e.target.value)}
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {films.map(film => (
-          <FilmCard
-            film={film}
-          />
-        ))}
-      </div>
+      {filteredFilms.length === 0 ? (
+        <div className="flex flex-col justify-center items-center gap-4 py-10">
+          <p className="text-lg text-gray-500">No movies found matching "{searchInputValue}"</p>
+          <Button
+            variant="outlined"
+            style={{ width: "fit-content" }}
+            onClick={clearFilter}
+          >
+            Clear All Filters
+          </Button>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {filteredFilms.map(film => (
+            <FilmCard
+              film={film}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
