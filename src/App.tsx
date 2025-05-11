d0f52a57d3748ead455c69e432f3a24b95764f84
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-11 11:05:17 -0300

    feat: axios api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import axios from "axios"
+import { useState } from "react"
+import type { Film } from "./types/Film"
+
 function App() {
+  const [films, setFilms] = useState<Film[]>([])
+
+  axios.get("https://ghibliapi.vercel.app/films/")
+    .then(res => {
+      setFilms(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Filmes</h1>
+
+      <ul>
+        {films.map(film => (
+          <li key={film.id}>{film.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
---
+++ src/types/Film.tsx
@@ -0,0 +1,11 @@
+export type Film = {
+  id: string,
+  image: string,
+  title: string,
+  release_date: string,
+  running_time: string,
+  description: string,
+  director: string,
+  producer: string,
+  rt_score: string,
+}
